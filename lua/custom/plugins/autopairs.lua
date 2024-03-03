bb986aa7bea8f16fd9e847c28598460550b34dea
lv8pv <6325009+lv8pv@users.noreply.github.com>
2024-03-04 00:41:55 +0100

    Added plugin autopairs
    Added `lua/custom/plugins/autopairs.lua`

---
+++ lua/custom/plugins/autopairs.lua
@@ -0,0 +1,14 @@
+-- This adds autopairs plugin (https://github.com/windwp/nvim-autopairs)
+return {
+  'windwp/nvim-autopairs',
+  -- Optional dependency
+  dependencies = { 'hrsh7th/nvim-cmp' },
+  config = function()
+    require('nvim-autopairs').setup {}
+    -- If you want to automatically add `(` after selecting a function or method
+    local cmp_autopairs = require 'nvim-autopairs.completion.cmp'
+    local cmp = require 'cmp'
+    cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
+  end,
+}
+-- vim: ts=4 sts=4 sw=4 et
