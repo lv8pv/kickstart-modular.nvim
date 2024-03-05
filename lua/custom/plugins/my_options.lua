2051f17c9a1bc4ac6e4758de6fea3305c968563d
lv8pv <6325009+lv8pv@users.noreply.github.com>
2024-03-05 13:11:21 +0100

    Updated my_options.lua
    Added config for opening :help files in a new full height buffer. Also
    removed specific autocmd for setting text width for markdown files

--- lua/custom/plugins/my_options.lua
+++ lua/custom/plugins/my_options.lua
@@ -27,37 +27,33 @@ local options = {
   sidescrolloff = 0, -- Same as scrolloff but for the sides. Only usefull when `nowrap`is set
 
   -- These are set in the main `lua/options.lua`
-  -- number = true,                           -- set numbered lines
-  -- mouse = "a",                             -- allow the mouse to be used in neovim
-  -- showmode = false,                        -- we don't need to see things like -- INSERT -- any more
+  -- number = true,               -- set numbered lines
+  -- mouse = "a",                 -- allow the mouse to be used in neovim
+  -- showmode = false,            -- we don't need to see things like -- INSERT -- any more
   -- clipboard = "unnamedplus",               -- allows neovim to access the system clipboard
-  -- breakindent = true,                      -- when breaking for linewrap indent corectly as line before
-  -- undofile = true,                         -- enable persistent undo
-  -- ignorecase = true,                       -- ignore case in search patterns
-  -- smartcase = true,                        -- smart case
-  -- signcolumn = "yes",                      -- always show the sign column, otherwise it would shift the text each time
-  -- timeoutlen = 250,                        -- time to wait for a mapped sequence to complete (in milliseconds)
-  -- updatetime = 300,                        -- faster completion (4000ms default)
-  -- splitbelow = true,                       -- force all horizontal splits to go below current window
-  -- splitright = true,                       -- force all vertical splits to go to the right of current window
+  -- breakindent = true,          -- when breaking for linewrap indent corectly as line before
+  -- undofile = true,             -- enable persistent undo
+  -- ignorecase = true,           -- ignore case in search patterns
+  -- smartcase = true,            -- smart case
+  -- signcolumn = "yes",          -- always show the sign column, otherwise it would shift the text each time
+  -- timeoutlen = 250,            -- time to wait for a mapped sequence to complete (in milliseconds)
+  -- updatetime = 300,            -- faster completion (4000ms default)
+  -- splitbelow = true,           -- force all horizontal splits to go below current window
+  -- splitright = true,           -- force all vertical splits to go to the right of current window
+  -- inccommand = 'split'         -- Preview substitutions live , as you type
+  -- cursorline = true,           -- highlight the current line
+  -- scrolloff = 10,              -- How many lines to keep the cursor away from the bottom of the screen
   --
-  -- Not sure I want this. Usually I think the editor looks messy with it but
-  -- for now I leave it in
-  -- list = true,                             -- Show how to display  white spaces in the editor
+  -- -- Show how to display  white spaces in the editor-
+  -- list = true,
   -- listchars = { tab = '» ', trail = '·', nbsp = '␣' }
   --
+  -- -- These are set in `lua/keymaps.lua` So we don't need it here.
+  -- hlsearch = true,             -- highlight all matches on previous search pattern
   --
-  -- inccommand = 'split'                     -- Preview substitutions live , as you type
-  -- cursorline = true,                       -- highlight the current line
-  -- scrolloff = 10,                          -- How many lines to keep the cursor away from the bottom of the screen
-  --
-  -- These are set in `lua/keymaps.lua` So we don't need it here.
-  -- hlsearch = true,                         -- highlight all matches on previous search pattern
-  --
-  -- These are set in `lua/kickstart/plugin/cmp.lua`
+  -- -- These are set in `lua/kickstart/plugin/cmp.lua`
   -- completeopt = { "menu", "menuone", "noinsert" },
-  -- I had also "noselect" set but not sure what its fore. I leave it here in
-  -- case for later use
+  -- -- I had also "noselect" set but not sure what its fore. I leave it here in case for later use
   -- completeopt = { "menuone", "noselect" }, -- mostly just for cmp
 }
 
@@ -76,9 +72,25 @@ vim.cmd [[
 ]]
 --
 -- Change a few settings for filetype `markdown`
+-- -- I reconsidered and came to the conclution that I prefere 79 and 80 for
+-- -- everything on my terminal. It is only for .md files that are going to
+-- -- github I need 120 and 121. For those files I use `-- vim modline` in
+-- -- the spesific files I need it. I left the code in here commented out if
+-- -- someone else need it
+-- --
+-- vim.cmd [[
+--     autocmd FileType markdown set textwidth=120|set colorcolumn=121
+-- ]]
+
+-- Added so that `:help` files opens in a new full height buffer.
+-- You can navigate between buffers using `shift + h|l` or `,` (in normal mode)
 vim.cmd [[
-    autocmd FileType markdown set textwidth=120|set colorcolumn=121
+    augroup help_as_buffer
+      autocmd!
+      autocmd FileType help :tabnew % | tabprevious | quit | tabnext 
+      autocmd FileType help set buflisted
+    augroup END
 ]]
 
 return {}
--- vim: ts=4 sts=4 sw=4 et
+-- vim: ts=2 sts=2 sw=2 et
