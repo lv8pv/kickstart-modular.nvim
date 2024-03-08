82b4f581c70c21c29b64faab75d3fe4672361e16
lv8pv <6325009+lv8pv@users.noreply.github.com>
2024-03-08 14:39:20 +0100

    Added plugin "nvim-tree"
    new file:   lua/custom/plugins/nvim-tree.lua
    <leader>t to open nvim.tree

---
+++ lua/custom/plugins/nvim-tree.lua
@@ -0,0 +1,91 @@
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+vim.g.nvim_tree_icons = {
+  default = '',
+  symlink = '',
+  git = {
+    unstaged = '',
+    staged = 'S',
+    unmerged = '',
+    renamed = '➜',
+    deleted = '',
+    untracked = 'U',
+    ignored = '◌',
+  },
+  folder = {
+    default = '',
+    open = '',
+    empty = '',
+    empty_open = '',
+    symlink = '',
+  },
+}
+return {
+  'nvim-tree/nvim-tree.lua',
+  version = '*',
+  lazy = false,
+  dependencies = {
+    'nvim-tree/nvim-web-devicons',
+  },
+  config = function()
+    require('nvim-tree').setup {
+      sort = {
+        sorter = 'case_sensitive',
+      },
+      view = {
+        width = 50,
+        side = 'left',
+      },
+      renderer = {
+        group_empty = true,
+      },
+      filters = {
+        dotfiles = false,
+        custom = {},
+      },
+      disable_netrw = true,
+      hijack_netrw = true,
+      open_on_tab = false,
+
+      hijack_cursor = false,
+      update_cwd = true,
+      diagnostics = {
+        enable = true,
+        icons = {
+          hint = '',
+          info = '',
+          warning = '',
+          error = '',
+        },
+      },
+      update_focused_file = {
+        enable = true,
+        update_cwd = true,
+        ignore_list = {},
+      },
+      system_open = {
+        cmd = nil,
+        args = {},
+      },
+      git = {
+        enable = true,
+        ignore = true,
+        timeout = 500,
+      },
+      trash = {
+        cmd = 'trash',
+        require_confirm = true,
+      },
+      actions = {
+        open_file = {
+          quit_on_open = true,
+          window_picker = {
+            enable = false,
+          },
+        },
+      },
+    }
+  end,
+}
+-- return {}
