4bf15a2a5e814372c162df1c5879d3d3a538735f
lv8pv <6325009+lv8pv@users.noreply.github.com>
2024-03-12 23:21:50 +0100

    bindings: Added more bindings cmp.lua
    Added more bindings to lua/custom/plugins/cmp.lua for luasnip. It is not
    sure I will keep these, as I'm really not sure how to use them or if
    they work. Problem I have is to move between fields when in luasnippets.
    
    modified:   lua/custom/plugins/cmp.lua

--- lua/custom/plugins/cmp.lua
+++ lua/custom/plugins/cmp.lua
@@ -23,7 +23,25 @@ return {
     require('luasnip').filetype_extend('htmldjango', { 'html' })
     -- loads vscode style snippets from installed plugins (e.g. friendly-snippets)
     require('luasnip.loaders.from_vscode').lazy_load()
-    luasnip.config.setup {}
+    luasnip.config.setup {
+      -- /lv8pv
+      vim.keymap.set({ 'i' }, '<C-K>', function()
+        luasnip.expand()
+      end, { silent = true }),
+      vim.keymap.set({ 'i', 's' }, '<C-L>', function()
+        luasnip.jump(1)
+      end, { silent = true }),
+      vim.keymap.set({ 'i', 's' }, '<C-J>', function()
+        luasnip.jump(-1)
+      end, { silent = true }),
+
+      vim.keymap.set({ 'i', 's' }, '<C-E>', function()
+        if luasnip.choice_active() then
+          luasnip.change_choice(1)
+        end
+      end, { silent = true }),
+      -- lv8pv/
+    }
     cmp.setup {
       completion = {
         completeopt = 'menu,menuone,preview,noselect',
