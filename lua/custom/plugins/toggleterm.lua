bcf366e262c1d1c2131fccd4f81ad2e341f55b56
lv8pv <6325009+lv8pv@users.noreply.github.com>
2024-03-20 19:01:42 +0100

    chore: Changed `toggleterm` to default float
    modified:   toggleterm.lua

--- lua/custom/plugins/toggleterm.lua
+++ lua/custom/plugins/toggleterm.lua
@@ -30,7 +30,7 @@ return {
       start_in_insert = true,
       insert_mappings = true,
       persist_size = true,
-      direction = 'vertical',
+      direction = 'float',
       close_on_exit = true,
       shell = vim.o.shell,
       float_opts = {
