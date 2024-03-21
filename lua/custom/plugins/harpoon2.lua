7bd88ad8353a0b48dd4200d014456398798b39fd
lv8pv <6325009+lv8pv@users.noreply.github.com>
2024-03-21 13:57:38 +0100

    chore: harpoon2.lua Changed default bindings
    Changed bindings from using <leader> to using Alt.
    
    modified:   harpoon2.lua

--- lua/custom/plugins/harpoon2.lua
+++ lua/custom/plugins/harpoon2.lua
@@ -24,35 +24,35 @@ return {
         desc = 'harpoon quick menu',
       },
       {
-        '<leader>1',
+        '<a-1>',
         function()
           require('harpoon'):list():select(1)
         end,
         desc = 'harpoon to file 1',
       },
       {
-        '<leader>2',
+        '<a-2>',
         function()
           require('harpoon'):list():select(2)
         end,
         desc = 'harpoon to file 2',
       },
       {
-        '<leader>3',
+        '<a-3>',
         function()
           require('harpoon'):list():select(3)
         end,
         desc = 'harpoon to file 3',
       },
       {
-        '<leader>4',
+        '<a-4>',
         function()
           require('harpoon'):list():select(4)
         end,
         desc = 'harpoon to file 4',
       },
       {
-        '<leader>5',
+        '<a-5>',
         function()
           require('harpoon'):list():select(5)
         end,
